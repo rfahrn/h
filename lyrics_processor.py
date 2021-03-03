16388a44871e53b01182c3b8c877eaca7e4cde74
Vera Lara Bernhard <veralara.bernhard@uzh.ch>
2021-03-03 12:05:25 +0100

    Upload lyrics_processor.py

---
+++ lyrics_processor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# lyrics_processor.py
+
+# University of Zurich
+# Department of Computational Linguistics
+
+# Authors: # TODO
+# Matriculation Numbers: #TODO
+
+
+from typing import List, Tuple
+
+
+def split_up_lines(lyrics_str: str) -> List[str]:
+    # TODO
+    pass
+
+
+def tokenize_lines(split_lyrics: List[str]) -> List[List[str]]:
+    # TODO
+    pass
+
+
+def filter_profanity(tokenized_lyrics: List[List[str]], filename: str) -> Tuple[List[List[str]], int]:
+    # TODO
+    pass
+
+
+def print_lyrics(processed_lyrics: List[List[str]]) -> None:
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    pass
